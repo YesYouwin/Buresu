ded02800f886f7c86711f895cfb61b793d0e2da5
YesYouwin <garenagaming32@gmail.com>
2026-03-06 21:21:36 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,20 @@
+import discord
+import os
+from discord.ext import commands
 
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
