b5e99be905ae729b93d7b2925c591a1898d45bac
YesYouwin <garenagaming32@gmail.com>
2026-03-07 00:06:32 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,6 +2,22 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+    
 
 TOKEN = os.getenv("TOKEN")
 
@@ -86,5 +102,5 @@ Players must inform beforehand so the event runs smoothly.
 
     await interaction.response.send_message(message)
 
-
+keep_alive()
 bot.run(TOKEN)
